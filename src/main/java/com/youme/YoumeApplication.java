3a24575b0bca0ecdec243cf56a92e4a5af326dbf
sheharyar12 <syar0052@gmail.com>
2018-06-26 22:06:13 -0500

    init

---
+++ src/main/java/com/youme/YoumeApplication.java
@@ -0,0 +1,12 @@
+package com.youme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoumeApplication.class, args);
+	}
+}
